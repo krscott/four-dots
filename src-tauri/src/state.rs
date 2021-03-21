beebe6d0217a8a1c693778e54173a750141cc92a
Kris Scott <kscott91@gmail.com>
2021-03-21 18:21:33 -0500

    Add win-state

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -5,7 +5,7 @@ use validator::{Validate, ValidationError};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize_repr, Deserialize_repr)]
 #[repr(u8)]
-pub enum Cell {
+pub enum MaybePlayer {
     None = 0,
     Player1Piece = 1,
     Player2Piece = 2,
@@ -18,26 +18,13 @@ pub enum Player {
     Player2 = 2,
 }
 
-impl From<Player> for Cell {
+impl From<Player> for MaybePlayer {
     fn from(player: Player) -> Self {
         match player {
-            Player::Player1 => Cell::Player1Piece,
-            Player::Player2 => Cell::Player2Piece,
+            Player::Player1 => MaybePlayer::Player1Piece,
+            Player::Player2 => MaybePlayer::Player2Piece,
         }
     }
-}
-
-#[derive(Debug, Clone, Validate, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-#[validate(schema(function = "validate_state"))]
-pub struct State {
-    #[validate(length(min = 1))]
-    cells: Vec<Cell>,
-    #[validate(range(min = 1))]
-    width: i32,
-    #[validate(range(min = 1))]
-    height: i32,
-    current_player: Player,
 }
 
 fn validate_state(state: &State) -> Result<(), ValidationError> {
@@ -50,6 +37,22 @@ fn validate_state(state: &State) -> Result<(), ValidationError> {
     }
 }
 
+#[derive(Debug, Clone, Validate, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[validate(schema(function = "validate_state"))]
+pub struct State {
+    #[validate(length(min = 1))]
+    cells: Vec<MaybePlayer>,
+    #[validate(range(min = 1))]
+    width: i32,
+    #[validate(range(min = 1))]
+    height: i32,
+    current_player: Player,
+    winning_segment: Option<(Player, Vec<(i32, i32)>)>,
+    player1_score: u16,
+    player2_score: u16,
+}
+
 impl State {
     pub fn new(width: i32, height: i32) -> anyhow::Result<Self> {
         Self::check_width_height(width, height)?;
@@ -57,10 +60,13 @@ impl State {
         let size = (width * height) as usize;
 
         Ok(Self {
-            cells: vec![Cell::None; size],
+            cells: vec![MaybePlayer::None; size],
             width,
             height,
             current_player: Player::Player1,
+            winning_segment: None,
+            player1_score: 0,
+            player2_score: 0,
         })
     }
 
@@ -76,20 +82,21 @@ impl State {
     }
 
     pub fn clear(&mut self) {
-        self.cells.fill(Cell::None);
+        self.cells.fill(MaybePlayer::None);
+        self.winning_segment = None;
     }
 
-    pub fn width(&self) -> i32 {
-        self.width
-    }
+    // pub fn width(&self) -> i32 {
+    //     self.width
+    // }
 
-    pub fn height(&self) -> i32 {
-        self.height
-    }
+    // pub fn height(&self) -> i32 {
+    //     self.height
+    // }
 
-    pub fn current_player(&self) -> Player {
-        self.current_player
-    }
+    // pub fn current_player(&self) -> Player {
+    //     self.current_player
+    // }
 
     pub fn is_valid_coord(&self, row: i32, column: i32) -> bool {
         row >= 0 && row < self.height && column >= 0 && column < self.width
@@ -103,26 +110,44 @@ impl State {
         }
     }
 
-    pub fn set(&mut self, row: i32, column: i32, value: Cell) -> anyhow::Result<()> {
+    pub fn set(&mut self, row: i32, column: i32, value: MaybePlayer) -> anyhow::Result<()> {
         let i = self.coord_to_index(row, column)?;
         self.cells[i] = value;
         Ok(())
     }
 
-    pub fn get(&self, row: i32, column: i32) -> anyhow::Result<Cell> {
+    pub fn get(&self, row: i32, column: i32) -> anyhow::Result<MaybePlayer> {
         let i = self.coord_to_index(row, column)?;
         Ok(self.cells[i])
     }
 
     pub fn is_set(&self, row: i32, column: i32) -> anyhow::Result<bool> {
-        Ok(self.get(row, column)? != Cell::None)
+        Ok(self.get(row, column)? != MaybePlayer::None)
     }
 
     pub fn put_piece_in_column(&mut self, column: i32) -> anyhow::Result<()> {
+        if self.winning_segment.is_some() {
+            return Err(anyhow!("Game is over"));
+        }
+
         for r in (0..self.height).rev() {
             if !self.is_set(r, column)? {
                 self.set(r, column, self.current_player.into())?;
+
+                self.winning_segment = self.find_winning_segment();
+
+                match self.winning_segment {
+                    None => {}
+                    Some((Player::Player1, _)) => {
+                        self.player1_score += 1;
+                    }
+                    Some((Player::Player2, _)) => {
+                        self.player2_score += 1;
+                    }
+                }
+
                 self.advance_turn();
+
                 return Ok(());
             }
         }
@@ -135,5 +160,55 @@ impl State {
             Player::Player1 => Player::Player2,
             Player::Player2 => Player::Player1,
         };
+    }
+
+    fn find_winning_segment(&self) -> Option<(Player, Vec<(i32, i32)>)> {
+        for r0 in 0..self.height {
+            for c0 in 0..self.height {
+                for (dr, dc) in &[(0, 1), (1, 1), (1, 0), (1, -1)] {
+                    match self.check_if_segment_is_win(r0, c0, *dr, *dc) {
+                        Some(seg) => return Some(seg),
+                        None => {}
+                    }
+                }
+            }
+        }
+
+        None
+    }
+
+    fn check_if_segment_is_win(
+        &self,
+        r0: i32,
+        c0: i32,
+        dr: i32,
+        dc: i32,
+    ) -> Option<(Player, Vec<(i32, i32)>)> {
+        const N: i32 = 4;
+
+        let player = match self.get(r0, c0) {
+            Ok(MaybePlayer::None) | Err(_) => {
+                return None;
+            }
+            Ok(MaybePlayer::Player1Piece) => Player::Player1,
+            Ok(MaybePlayer::Player2Piece) => Player::Player2,
+        };
+
+        let segment_iter = (0..N).map(|i| (r0 + dr * i, c0 + dc * i));
+
+        let cmp_player_piece = player.into();
+
+        // The last piece is more likely to be off the board, so checking it first is usually faster
+        for (r, c) in segment_iter.clone().rev().take((N - 1) as usize) {
+            if self.get(r, c).unwrap_or(MaybePlayer::None) != cmp_player_piece {
+                return None;
+            }
+        }
+
+        let segment = (player, segment_iter.collect());
+
+        println!("{:?}", segment);
+
+        Some(segment)
     }
 }
